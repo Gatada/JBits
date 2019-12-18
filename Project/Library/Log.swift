33553f92302f952a1e50ec81fffa7c8d7e5b6fc1
Johan Basberg <johan@basberg.com>
2019-12-18 14:12:25 +0000

    Removed unneccessary parameter for os()
    Added comments.
    Removed terminator from os logging calls as they cannot be used.

--- Project/Library/Log.swift
+++ Project/Library/Log.swift
@@ -26,13 +26,13 @@ public enum Log {
         ///
         /// Logs of this kind are usually temporary, and are therefore
         /// removed after the feature has been completely implemented.
-        case `default`
+        case `default` = "Default"
         
         /// The `info` log level used to output state information.
         ///
         /// Info logs will usually be left in the application even
         /// after the feature is fully implemented.
-        case info
+        case info = "Info"
         
         /// The `debug` log level is used to log state expectations
         /// to prevent errors.
@@ -52,7 +52,7 @@ public enum Log {
         ///
         /// Debug output will usually be removed after the feature has been
         /// fully implemented.
-        case debug
+        case debug = "Debug"
         
         /// A `fault` indicates unintended behaviour usually as a result
         /// of an error.
@@ -63,16 +63,16 @@ public enum Log {
         ///
         /// Fault logs are useful for the inevitable refactoring and should
         /// therefore not be removed.
-        case fault
+        case fault = "Fault"
         
         /// A failure is the formal inability to fulfill performance requirements.
         ///
         /// Tests can be used to prevent failure. Assertions can also be
         /// used to catch or inform the developer about failures even
         /// before testing begins.
-        case failure
+        case failure = "Failure"
         
-        
+        /// A suitable emoji that marks the beginning of a log message.
         var emoji: StaticString {
             switch self {
             case .default:
@@ -88,6 +88,7 @@ public enum Log {
             }
         }
         
+        /// Mapping a Category to a suitable OSLog type.
         var osLogEquivalent: OSLog {
             switch self {
             case .default:
@@ -103,6 +104,10 @@ public enum Log {
             }
         }
         
+        /// Maps the Category to a suitable OSLogType.
+        ///
+        /// Only some of the existing OSLogTypes seem to appear in
+        /// the Console, which is handled by this mapping.
         var osLogTypeEquivalent: OSLogType {
             switch self {
             case .default, .info, .debug:
@@ -121,9 +126,18 @@ public enum Log {
 
     // MARK: - Private Helpers
 
-    /// Prints the message received in the Xcode debug area.
+    /// Prints the messages received in the Xcode debug area.
+    ///
+    /// All the messages are printed on a single line, separated with a space character,
+    /// and at the end of the message the terminator is appended.
+    ///
+    /// - Parameters:
+    ///   - messages: An array of strings, each resulting in a message sent to the OS logging system.
+    ///   - terminator: The string appended to the message. By default this is `\n`.
+    ///   - log: Use this to group logs into a suitable `Category`.
+    ///   - subsystem: A string describing a subsystem. Default value is the main bundle identifier.
     private static func debugAreaPrint(_ messages: [String], terminator: String, log: Log.Category, subsystem: String) -> Bool {
-        print("\(log.emoji) \(timestamp()) \(subsystem) –", terminator: "")
+        print("\(log.emoji) \(timestamp()) \(log) \(subsystem) –", terminator: "")
         for message in messages {
             print(" " + message, terminator: "")
         }
@@ -131,8 +145,9 @@ public enum Log {
         return true
     }
     
-    /// Calling this will generate a log message that will be collected by the Console
-    /// app on your Mac.
+    /// Call with one or more messages to generate an equal number of messages in the OS logging system.
+    ///
+    /// Each string in the messages array will generate one message in the Console.
     ///
     /// To review the resulting log messages please launch the Console app on your Mac.
     /// Make sure you have selected the correct device when browsing the messages.
@@ -140,9 +155,14 @@ public enum Log {
     /// - Note:
     /// To enable logging with the OS logging system the `OS_ACTIVITY_MODE` option found
     /// in the the build scheme cannot be disabled.
-    private static func osPrint(_ messages: [String], terminator: String, log: Log.Category, subsystem: String) {
+    ///
+    /// - Parameters:
+    ///   - messages: An array of strings, each resulting in a message sent to the OS logging system.
+    ///   - log: Use this to group logs into a suitable `Category`.
+    ///   - subsystem: A string describing a subsystem. Default value is the main bundle identifier.
+    private static func osPrint(_ messages: [String], log: Log.Category, subsystem: String) {
         for message in messages {
-            os_log("%{private}@", log: log.osLogEquivalent, type: log.osLogTypeEquivalent, message)
+            os_log("%{private}@", log: log.osLogEquivalent, type: log.osLogTypeEquivalent, "\(log.emoji) \(subsystem) - \(message)")
         }
     }
     
@@ -159,28 +179,57 @@ public enum Log {
     /// Use to temporary log events in the Xcode debug area (the console).
     ///
     /// These calls will be completely removed for release or any non-debugging build.
+    ///
+    /// - Parameters:
+    ///   - messages: A variadic parameter of strings to print in the debug area.
+    ///   - terminator: The string appended to the message. By default this is `\n`.
+    ///   - log: Use this to group logs into a suitable `Category`.
+    ///   - subsystem: A string describing a subsystem. Default value is the main bundle identifier.
     public static func tmp(_ messages: String..., terminator: String = "\n", log: Log.Category = .default, subsystem: String = Log.mainBundle) {
         assert(Log.debugAreaPrint(messages, terminator: terminator, log: log, subsystem: subsystem))
     }
     
     /// Use to send messages to the OS logging system.
     ///
-    /// The messages received will be retained in a cyclic buffer managed
-    /// by the operating system.
+    /// The messages received will be retained in a ring buffer managed
+    /// by the operating system. All received log messages can be exported on the device,
+    /// however the file is usually very large (probably too large to send by email).
+    ///
+    /// To review the log messages in real time please launch the Console app on your Mac.
+    /// Make sure you have selected the correct device when browsing the messages.
     ///
     /// - Note:
     /// To enable logging with the OS logging system the `OS_ACTIVITY_MODE` option found
     /// in the the build scheme cannot be disabled.
-    public static func os(_ messages: String..., terminator: String = "\n", log: Log.Category = .default, subsystem: String = Log.mainBundle) {
-        osPrint(messages, terminator: terminator, log: log, subsystem: subsystem)
+    ///
+    /// - Parameters:
+    ///   - messages: A variadic parameter of strings to print in the debug area.
+    ///   - log: Use this to group logs into a suitable `Category`.
+    ///   - subsystem: A string describing a subsystem. Default value is the main bundle identifier.
+    public static func os(_ messages: String..., log: Log.Category = .default, subsystem: String = Log.mainBundle) {
+        osPrint(messages, log: log, subsystem: subsystem)
     }
     
     /// Send a message to both the Xcode debug area and the OS logging system.
+    ///
+    /// The messages received will be retained in a ring buffer managed
+    /// by the operating system. All received log messages can be exported on the device,
+    /// however the file is usually very large (probably too large to send by email).
+    ///
+    /// To review the log messages in real time please launch the Console app on your Mac.
+    /// Make sure you have selected the correct device when browsing the messages.
+    ///
     /// - Note:
     /// To enable logging with the OS logging system the `OS_ACTIVITY_MODE` option found
     /// in the the build scheme cannot be disabled.
+    ///
+    /// - Parameters:
+    ///   - messages: A variadic parameter of strings to print in the debug area.
+    ///   - terminator: The string appended to the message showing up in the debug area in Xcode. By default this is `\n`.
+    ///   - log: Use this to group logs into a suitable `Category`.
+    ///   - subsystem: A string describing a subsystem. Default value is the main bundle identifier.
     public static func both(_ messages: String..., terminator: String = "\n", log: Log.Category = .default, subsystem: String = Log.mainBundle) {
-        osPrint(messages, terminator: terminator, log: log, subsystem: subsystem)
+        osPrint(messages, log: log, subsystem: subsystem)
         assert(Log.debugAreaPrint(messages, terminator: "\n", log: log, subsystem: subsystem))
     }
 
