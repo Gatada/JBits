24e64aa239b01ddabd6bd30ea0fffd59f71c835f
Johan Basberg <johan@basberg.com>
2020-02-07 10:32:10 +0000

    Improved comments

--- Project/Decorators/Array.swift
+++ Project/Decorators/Array.swift
@@ -15,9 +15,10 @@ extension Array {
     
     /// Parses the array to executes the `operation` on each element that returns `true` for `predicate`.
     ///
+    /// Any error thrown during execution of either `predicate` or `operation` will be rethrown to caller.
     /// - Parameters:
-    ///   - predicate: The check that has to return `true` using the current element for `operation` to be executed.
-    ///   - operation: This closure is executed using the received `element` as input whenever the `predicate` returns `true`.
+    ///   - predicate: The check that has to return `true` using the current element for the `operation` closure to be executed.
+    ///   - operation: This closure that will be executed when the `predicate` returns `true` for an element.
     public func forEach(where predicate: ((Element) throws -> Bool), do operation: ((Element) throws -> Void)) rethrows {
         for element in self where try predicate(element) {
             try operation(element)
