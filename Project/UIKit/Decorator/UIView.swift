c4c361d10cb298005f86ac956f55fc5fa05c3215
Johan Basberg <johan@basberg.com>
2020-02-20 10:03:40 +0000

    Added recommended parameters for throb.

--- Project/UIKit/Decorator/UIView.swift
+++ Project/UIKit/Decorator/UIView.swift
@@ -29,6 +29,10 @@ public extension UIView {
     /// Briefly scales the view up and back down, like
     /// a the beating or throbbing of a heart.
     ///
+    /// The default values creates a visible throb, not really useful as a tap or refresh reponse.
+    /// If the throb is being used to visualize a content refresh, then you may find a duration of 0.05
+    /// and a scale of 1.15 to be more suitable.
+    ///
     /// - Parameters:
     ///   - duration: The default duration of the entire animation, default is 0.1 seconds.
     ///   - scale: How much the view will be scaled, default is 1.3.
