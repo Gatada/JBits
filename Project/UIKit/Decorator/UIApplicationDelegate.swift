d43cf8ef98dfb25eb7cfa62d448b49d306b52812
Johan Basberg <johan@basberg.com>
2020-03-09 15:38:27 +0000

    Joining random game no longer clears board

---
+++ Project/UIKit/Decorator/UIApplicationDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  UIApplicationDelegate.swift
+//  TugOfWar
+//
+//  Created by Basberg, Johan on 22/03/2019.
+//  Copyright © 2019 Basberg, Johan. All rights reserved.
+//
+
+import UIKit
+
+public extension UIApplicationDelegate {
+
+    
+    /// Returns current version and build number as a string.
+    ///
+    /// It fetches the information from the info dictionary from
+    /// the main bundle.
+    ///
+    static var versionBuild: String {
+        let version: String = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") as! String
+        let build: String = Bundle.main.infoDictionary?[kCFBundleVersionKey as String] as! String
+        return "\(version) (\(build))"
+    }    
+}
