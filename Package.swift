a2ce398b611a07f27042aa7ea56e1eae42a00e01
Johan Basberg <johan@basberg.com>
2020-03-16 14:09:31 +0000

    Removed a space.

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
             name: "JBits",
             targets: ["JBits"]),
     ],
-    dependencies: [ ],
+    dependencies: [],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
