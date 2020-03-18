594303bb42b251b6f1280288d96884acaf35db6e
Johan Basberg <johan@basberg.com>
2020-03-18 16:29:30 +0000

    Slide show tutorial finished

--- Package.swift
+++ Package.swift
@@ -8,7 +8,8 @@ let package = Package(
 	 platforms: [
 			  .macOS(.v10_13),
 			  .iOS(.v12),
-			  .watchOS(.v5)
+			  .watchOS(.v5),
+              .tvOS(.v10)
 		 ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
