2aabc0deeef2db74fb7f73137bda34e90cae1ec7
Johan Basberg <johan@basberg.com>
2020-05-19 10:16:23 +0100

    Added match mapper Tweaked UI Removed SpriteKit Game

---
+++ Sources/JBits/Foundation/Class/InsetLabel.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  
+//
+//  Created by Johan Basberg on 19/05/2020.
+//
+
+import UIKit
+
+@IBDesignable class InsetLabel: UILabel {
+    
+    // MARK: - Properties
+    
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var leftInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
+    @IBInspectable var rightInset: CGFloat = 0.0
+
+    var insets: UIEdgeInsets {
+        get {
+            return UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+        }
+        set {
+            topInset = newValue.top
+            leftInset = newValue.left
+            bottomInset = newValue.bottom
+            rightInset = newValue.right
+        }
+    }
+    
+    
+    // MARK: - Drawing
+
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: rect.inset(by: insets))
+    }
+
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var adjSize = super.sizeThatFits(size)
+        adjSize.width += leftInset + rightInset
+        adjSize.height += topInset + bottomInset
+
+        return adjSize
+    }
+
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.width += leftInset + rightInset
+        contentSize.height += topInset + bottomInset
+
+        return contentSize
+    }
+}
